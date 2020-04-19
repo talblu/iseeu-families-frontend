ce0a0b08f4c8623dc643a05f53486ddf05e39a27
talblu <bluman123@hotmail.com>
2020-04-19 15:19:46 +0300

    Take hospital from URL fix

--- ISeeU_Families_FrontEnd/js/main.js
+++ ISeeU_Families_FrontEnd/js/main.js
@@ -1,4 +1,4 @@
-const hospital = location.href.substring(8, location.href.indexOf(','));
+const hospital = location.href.substring(8, location.href.indexOf('.'));
 
 const dataToSend = {};
 dataToSend.hospital = hospital;
